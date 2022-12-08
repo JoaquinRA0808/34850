929e2ab8059738c24890f404fd00b6d70e8eecf8
Joaquin Rodriguez <joaquin.rodriguez@live.com>
2022-12-08 08:19:40 -0300

    ejs

---
+++ Documents/my-app/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import ItemListContainer from './Components/ItemListContainer/ItemListContainer';
+import NavBar from './Components/NavBar/NavBar';
+
+function App() {
+  return (
+    <div className="App">
+      <NavBar />
+      <ItemListContainer greeting='Este es un mensaje de ItemListContainer' />
+    </div>
+  );
+}
+
+export default App;
